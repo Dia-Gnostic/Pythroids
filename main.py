c118c83e703b4846958124ed778929318416af7c
VoiD101GamErS <richiemc99@gmail.com>
2025-07-31 09:34:39 -0400

    I have a player now yay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame as pg
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS, PLAYER_RADIUS
+from player import Player
 
 def main():
 	print("Starting Asteroids!")
@@ -7,6 +8,10 @@ def main():
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	pg.init()
 	screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pg.time.Clock()
+	dt = 0
+
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
 
 	while True:
 		for event in pg.event.get():
@@ -14,7 +19,10 @@ def main():
 				return
 
 		screen.fill("black")
+		player.draw(screen)
 		pg.display.flip()
+		dt = clock.tick(60) / 1000
+		print(dt)
 
 if __name__ == "__main__":
     main()
